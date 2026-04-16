23a1b2378e8c3d37b9d29eff7bf78917eb23173b
isaac20439 <cabarcasisaac21@gmail.com>
2026-04-16 17:50:09 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "Restaurant",
+  "name": "Crosh House",
+  "address": {
+    "@type": "PostalAddress",
+    "addressLocality": "Barranquilla"
+  },
+  "servesCuisine": "Fast Food",
+  "openingHours": "Mo-Su 15:00-23:30"
+}
+</script>
